59b46a615226cbf2ba6ae7800896edd4dc980c0d
lahe8230 <lahe8230@bht-berlin.de>
2026-03-24 11:00:51 +0100

    inverted control flow demo

--- backend/src/main/java/com/sharedplanner/studyplanner/StudyplannerApplication.java
+++ backend/src/main/java/com/sharedplanner/studyplanner/StudyplannerApplication.java
@@ -2,12 +2,24 @@ package com.sharedplanner.studyplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class StudyplannerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudyplannerApplication.class, args);
-	}
+    public StudyplannerApplication() {
+        System.out.println("-<2>--> Constructor called");
+    }
 
+    public static void main(String[] args) {
+        System.out.println("-<1>--> before Spring has been started");
+        SpringApplication.run(StudyplannerApplication.class, args);
+        System.out.println("-<4>--> after Spring has exited");
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        System.out.println("-<3>--> doSomethingAfterStartup()");
+    }
 }
