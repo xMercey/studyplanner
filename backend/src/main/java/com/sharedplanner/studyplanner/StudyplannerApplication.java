1618613916b4b79b8316a5ee339e1c1ba4470417
lahe8230 <lahe8230@bht-berlin.de>
2026-03-24 10:16:14 +0100

    SpringBoot Projekt aufgesetzt

---
+++ backend/src/main/java/com/sharedplanner/studyplanner/StudyplannerApplication.java
@@ -0,0 +1,13 @@
+package com.sharedplanner.studyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyplannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sharedplanner/studyplanner/StudyplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharedplanner.studyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
